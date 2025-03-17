bf644d62ae0a64e066ee11802c94d0c1eca26011
bastien-muraccioli <bastien.muraccioli.pro@gmail.com>
2025-03-17 19:47:41 +0900

    Initial commit

--- src/SuperTwisting.cpp
+++ src/SuperTwisting.cpp
@@ -9,7 +9,61 @@ SuperTwisting::~SuperTwisting() = default;
 
 void SuperTwisting::init(mc_control::MCGlobalController & controller, const mc_rtc::Configuration & config)
 {
-  mc_rtc::log::info("SuperTwisting::init called with configuration:\n{}", config.dump(true, true));
+    auto & ctl = static_cast<mc_control::MCGlobalController &>(controller);
+
+    auto & robot = ctl.robot(ctl.robots()[0].name());
+    auto & realRobot = ctl.realRobot(ctl.robots()[0].name());
+    auto & rjo = robot.refJointOrder();
+
+    if(!robot.hasDevice<mc_rbdyn::ExternalTorqueSensor>("externalTorqueSensor"))
+    {
+        mc_rtc::log::error_and_throw<std::runtime_error>(
+            "[ExternalForcesEstimator][Init] No \"ExternalTorqueSensor\" with the name \"externalTorqueSensor\" found in "
+            "the robot module, please add one to the robot's RobotModule.");
+    }
+    extTorqueSensor = &robot.device<mc_rbdyn::ExternalTorqueSensor>("externalTorqueSensor");
+
+    referenceFrame = config("reference_frame", (std::string) "");
+
+    dt_ = ctl.timestep();
+    counter_ = 0.0;
+
+    jointNumber = ctl.robot(ctl.robots()[0].name()).refJointOrder().size();
+
+    tau_m.setZero(jointNumber);
+    tau_ext_hat.setZero(jointNumber);
+    tau_ext_hat_dot.setZero(jointNumber);
+    inertiaMatrix.resize(jointNumber, jointNumber);
+    tau_ext.setZero(jointNumber);
+
+    Eigen::VectorXd qdot(jointNumber);
+    for(size_t i = 0; i < jointNumber; i++)
+    {
+        qdot[i] = robot.alpha()[robot.jointIndexByName(rjo[i])][0];
+    }
+
+    jac = rbd::Jacobian(robot.mb(), referenceFrame);
+    jTranspose = jac.jacobian(robot.mb(), robot.mbc());
+    jTranspose.transposeInPlace();
+    coriolis = new rbd::Coriolis(robot.mb());
+    forwardDynamics = rbd::ForwardDynamics(robot.mb());
+
+    forwardDynamics.computeH(robot.mb(), robot.mbc());
+    inertiaMatrix = forwardDynamics.H() - forwardDynamics.HIr();
+    p = inertiaMatrix * qdot;
+    p_hat = Eigen::VectorXd::Zero(jointNumber);
+    p_error = p - p_hat;
+    auto coriolisMatrix = coriolis->coriolis(realRobot.mb(), realRobot.mbc());
+    auto coriolisGravityTerm = forwardDynamics.C(); //C*qdot + g
+    gamma = tau_m + (coriolisMatrix + coriolisMatrix.transpose()) * qdot - coriolisGravityTerm;
+
+    externalForcesFT = Eigen::Vector6d::Zero();
+
+    addGui(ctl);
+    addLog(ctl);
+    addPlot(ctl);
+
+    mc_rtc::log::info("SuperTwisting::init called with configuration:\n{}", config.dump(true, true));
 }
 
 void SuperTwisting::reset(mc_control::MCGlobalController & controller)
@@ -17,9 +71,24 @@ void SuperTwisting::reset(mc_control::MCGlobalController & controller)
   mc_rtc::log::info("SuperTwisting::reset called");
 }
 
-void SuperTwisting::before(mc_control::MCGlobalController &)
+void SuperTwisting::before(mc_control::MCGlobalController & controller)
 {
-  mc_rtc::log::info("SuperTwisting::before");
+    auto & ctl = static_cast<mc_control::MCGlobalController &>(controller);
+    auto & robot = ctl.robot(ctl.robots()[0].name());
+    auto & realRobot = ctl.realRobot(ctl.robots()[0].name());
+    counter_ += dt_;
+    computeMomemtum(ctl);
+    tau_ext = tau_ext_hat + jTranspose*externalForcesFT;
+    if(plugin_active)
+    {
+        extTorqueSensor->torques(tau_ext);
+    }
+    else
+    {
+        Eigen::VectorXd zero = Eigen::VectorXd::Zero(jointNumber);
+        extTorqueSensor->torques(zero);
+    }
+    // mc_rtc::log::info("SuperTwisting::before");
 }
 
 void SuperTwisting::after(mc_control::MCGlobalController & controller)
@@ -31,9 +100,171 @@ mc_control::GlobalPlugin::GlobalPluginConfiguration SuperTwisting::configuration
 {
   mc_control::GlobalPlugin::GlobalPluginConfiguration out;
   out.should_run_before = true;
-  out.should_run_after = true;
+  out.should_run_after = false;
   out.should_always_run = true;
   return out;
+}
+
+void SuperTwisting::computeMomemtum(mc_control::MCGlobalController & controller)
+{
+    auto & ctl = static_cast<mc_control::MCGlobalController &>(controller);
+
+    if(ctl.robot().encoderVelocities().empty())
+    {
+        return;
+    }
+
+    auto & robot = ctl.robot();
+    auto & realRobot = ctl.realRobot(ctl.robots()[0].name());
+
+    auto & rjo = realRobot.refJointOrder();
+    auto R = controller.robot().bodyPosW(referenceFrame).rotation();
+
+    Eigen::VectorXd qdot(jointNumber);
+    rbd::paramToVector(realRobot.alpha(), qdot);
+    tau_m = Eigen::VectorXd::Map(realRobot.jointTorques().data(), realRobot.jointTorques().size());
+    // mc_rtc::log::info("[SuperTwisting] qdot: {}, tau_m: {}", qdot, tau_m);
+    forwardDynamics.computeC(realRobot.mb(), realRobot.mbc());
+    forwardDynamics.computeH(realRobot.mb(), realRobot.mbc());
+    auto coriolisMatrix = coriolis->coriolis(realRobot.mb(), realRobot.mbc());
+    auto coriolisGravityTerm = forwardDynamics.C(); //C*qdot + g
+    jTranspose = jac.jacobian(realRobot.mb(), realRobot.mbc());
+    jTranspose.transposeInPlace();
+    auto sva_EF_FT = realRobot.forceSensor("EEForceSensor").wrenchWithoutGravity(realRobot);
+    externalForcesFT.head(3) = R.transpose() * externalForcesFT.head(3);
+    externalForcesFT.tail(3) = R.transpose() * externalForcesFT.tail(3);
+    gamma = tau_m + (coriolisMatrix + coriolisMatrix.transpose()) * qdot - coriolisGravityTerm + jTranspose*externalForcesFT; //gamma = tau_m -g + C^T*qdot + J^T*F_ext
+    inertiaMatrix = forwardDynamics.H() - forwardDynamics.HIr();
+    // x_hat_dot = Gamma + γ1*sqrt(|x - x_hat|)*Sign(x - x_hat) + d_hat
+    Eigen::VectorXd p_hat_dot = gamma + gamma1*(p_error).cwiseAbs().cwiseSqrt().cwiseProduct(Sign(p_error)) + alpha1*(p_error) + tau_ext_hat;
+    p_hat += p_hat_dot*dt_;
+
+    // d_hat_dot = γ2*Sign(x - x_hat)
+    // tau_ext_hat_dot = (gamma_2+gamma_3*p_error.cwiseAbs())*Sign(p_error);
+    tau_ext_hat_dot = (gamma2*p_error.cwiseAbs())*Sign(p_error) + alpha2*p_error;
+    // integrate tau_ext_hat_dot to get tau_ext_hat
+    tau_ext_hat += tau_ext_hat_dot*dt_;
+    p = inertiaMatrix * qdot;
+    p_error = p - p_hat;
+}
+
+double SuperTwisting::sign(double x)
+{
+    if (x>0) return 1;
+    else if (x<0) return -1;
+    else return 0;
+}
+
+Eigen::VectorXd SuperTwisting::Sign(Eigen::VectorXd x)
+{
+    Eigen::VectorXd y(x.size());
+    for (int i=0; i<x.size(); i++)
+    {
+        y[i] = sign(x[i]);
+    }
+    return y;
+}
+
+void SuperTwisting::addPlot(mc_control::MCGlobalController & ctl)
+{
+    auto & gui = *ctl.controller().gui();
+    //Momentum
+    gui.addPlot(
+        "Momentum",
+        mc_rtc::gui::plot::X(
+            "t", [this]() { return counter_; }),
+        mc_rtc::gui::plot::Y(
+            "p(t)", [this]() { return p[jointShown]; }, mc_rtc::gui::Color::Red),
+        mc_rtc::gui::plot::Y(
+            "p_hat(t)", [this]() { return p_hat[jointShown]; }, mc_rtc::gui::Color::Green)
+        );
+    //Momentum error
+    gui.addPlot(
+        "Momentum error",
+        mc_rtc::gui::plot::X(
+            "t", [this]() { return counter_; }),
+        mc_rtc::gui::plot::Y(
+            "p_error(t)", [this]() { return p_error[5]; }, mc_rtc::gui::Color::Red)
+        );
+    gui.addPlot(
+        "Torque estimation",
+        mc_rtc::gui::plot::X("t", [this]() { return counter_; }),
+        mc_rtc::gui::plot::Y("tau_ext_hat(t)", [this]() { return -tau_ext_hat[jointShown]; }, mc_rtc::gui::Color::Red),
+        mc_rtc::gui::plot::Y("tau_m(t)", [this]() { return tau_m[jointShown]; }, mc_rtc::gui::Color::Green),
+        mc_rtc::gui::plot::Y("tau_ext(t)", [this]() { return -tau_ext[jointShown]; }, mc_rtc::gui::Color::Blue)
+    );
+    gui.addPlot(
+        "Gamma",
+        mc_rtc::gui::plot::X("t", [this]() { return counter_; }),
+        mc_rtc::gui::plot::Y("gamma(t)", [this]() { return gamma[jointShown]; }, mc_rtc::gui::Color::Red)
+    );
+}
+
+void SuperTwisting::addGui(mc_control::MCGlobalController & ctl)
+{
+    auto & gui = *ctl.controller().gui();
+    gui.addElement({"Plugins", "SuperTwisting"},
+        mc_rtc::gui::Checkbox(
+            "Is estimation feedback active", plugin_active));
+
+    gui.addElement({"Plugins", "SuperTwisting"},
+        mc_rtc::gui::NumberInput(
+            "jointShown", [this]() { return jointShown; },
+            [this](int joint)
+            {
+                this->jointShown = joint;
+            }));
+    gui.addElement({"Plugins", "SuperTwisting"},
+        mc_rtc::gui::NumberInput(
+            "gamma2", [this]() { return gamma2; },
+            [this](double gain)
+            {
+                this->gamma2 = gain;
+                tau_ext_hat.setZero(jointNumber);
+                tau_ext_hat_dot.setZero(jointNumber);
+                p_hat.setZero(jointNumber);
+            }));
+    gui.addElement({"Plugins", "SuperTwisting"},
+        mc_rtc::gui::NumberInput(
+            "gamma1", [this]() { return gamma1; },
+            [this](double gain)
+            {
+                this->gamma1 = gain;
+                tau_ext_hat.setZero(jointNumber);
+                tau_ext_hat_dot.setZero(jointNumber);
+                p_hat.setZero(jointNumber);
+            }));
+    gui.addElement({"Plugins", "SuperTwisting"},
+        mc_rtc::gui::NumberInput(
+            "alpha2", [this]() { return alpha2; },
+            [this](double gain)
+            {
+                this->alpha2 = gain;
+                tau_ext_hat.setZero(jointNumber);
+                tau_ext_hat_dot.setZero(jointNumber);
+                p_hat.setZero(jointNumber);
+            }));
+    gui.addElement({"Plugins", "SuperTwisting"},
+        mc_rtc::gui::NumberInput(
+            "alpha1", [this]() { return alpha1; },
+            [this](double gain)
+            {
+                this->alpha1 = gain;
+                tau_ext_hat.setZero(jointNumber);
+                tau_ext_hat_dot.setZero(jointNumber);
+                p_hat.setZero(jointNumber);
+            }));
+}
+
+void SuperTwisting::addLog(mc_control::MCGlobalController & ctl)
+{
+    ctl.controller().logger().addLogEntry("SuperTwisting_p", [this]() { return p; });
+    ctl.controller().logger().addLogEntry("SuperTwisting_p_hat", [this]() { return p_hat; });
+    ctl.controller().logger().addLogEntry("SuperTwisting_p_error", [this]() { return p_error; });
+    ctl.controller().logger().addLogEntry("SuperTwisting_tau_ext_hat", [this]() { return tau_ext_hat; });
+    ctl.controller().logger().addLogEntry("SuperTwisting_tau_ext_hat_dot", [this]() { return tau_ext_hat_dot; });
+    ctl.controller().logger().addLogEntry("SuperTwisting_gamma", [this]() { return gamma; });
+    ctl.controller().logger().addLogEntry("SuperTwisting_tau_ext", [this]() { return tau_ext; });
 }
 
 } // namespace mc_plugin
--- src/SuperTwisting.h
+++ src/SuperTwisting.h
@@ -6,6 +6,15 @@
 
 #include <mc_control/GlobalPlugin.h>
 
+#include <RBDyn/Coriolis.h>
+#include <RBDyn/FA.h>
+#include <RBDyn/FK.h>
+#include <RBDyn/FV.h>
+#include <RBDyn/MultiBody.h>
+#include <RBDyn/MultiBodyConfig.h>
+
+#include <mc_rbdyn/ExternalTorqueSensor.h>
+
 namespace mc_plugin
 {
 
@@ -15,15 +24,52 @@ struct SuperTwisting : public mc_control::GlobalPlugin
 
   void reset(mc_control::MCGlobalController & controller) override;
 
-  void before(mc_control::MCGlobalController &) override;
+  void before(mc_control::MCGlobalController & controller) override;
 
   void after(mc_control::MCGlobalController & controller) override;
+
+  double sign(double x);
+  Eigen::VectorXd Sign(Eigen::VectorXd x);
+  void computeMomemtum(mc_control::MCGlobalController & controller);
+  bool collisionDetection(mc_control::MCGlobalController & ctl);
+
+  void addPlot(mc_control::MCGlobalController & ctl);
+  void addLog(mc_control::MCGlobalController & ctl);
+  void addGui(mc_control::MCGlobalController & ctl);
 
   mc_control::GlobalPlugin::GlobalPluginConfiguration configuration() override;
 
   ~SuperTwisting() override;
 
 private:
+
+  std::string referenceFrame;
+  bool plugin_active = false;
+  double dt_; // Time step
+  int jointNumber;
+  int jointShown = 0;
+  double counter_;
+
+  rbd::Jacobian jac;
+  rbd::Coriolis * coriolis;
+  rbd::ForwardDynamics forwardDynamics;
+  Eigen::VectorXd gamma;
+  Eigen::MatrixXd inertiaMatrix;
+  Eigen::MatrixXd jTranspose;
+  Eigen::VectorXd p; //momentum
+  Eigen::VectorXd p_hat; //Estimated momentum
+  Eigen::VectorXd p_error; //Momentum error
+  Eigen::VectorXd tau_m;
+  Eigen::VectorXd tau_ext_hat; //Estimated external torque without the FT_sensor
+  Eigen::VectorXd tau_ext_hat_dot; //Estimated external torque derivative without the FT_sensor
+  Eigen::VectorXd tau_ext; //Estimated external torque of all the system
+  double gamma2 = 100.0;
+  double gamma1 = 21;
+  double alpha2 = 100; //100 (adaptive)
+  double alpha1 = 100; //100 (adaptive)
+
+  Eigen::Vector6d externalForcesFT;
+  mc_rbdyn::ExternalTorqueSensor * extTorqueSensor;
 };
 
 } // namespace mc_plugin
